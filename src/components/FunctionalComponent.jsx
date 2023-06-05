760298054143da3387f08faaded84802dda659ff
Noordeep Kaur <noordeep29oct@gmail.com>
2023-06-05 19:21:43 +0530

    Components

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//function FunctionalComponent()
+
+const FunctionalComponent=()=>{
+    return(
+        <div>
+            <p>This is a Functional Component</p>
+        </div>
+    )
+}
+
+export default FunctionalComponent;
